8c2c29f4e0a320d9f7a65a3b8bd76eb341297573
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-03-08 18:02:08 -0300

    08.03 tests

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,9 @@
+// agrupar testes
+describe("Creiar categoria", () => {
+  it("espero que 2 + 2 seja 4", () => {
+    const soma = 2 + 2;
+    const resultado = 4;
+
+    expect(soma).toBe(resultado);
+  });
+});
