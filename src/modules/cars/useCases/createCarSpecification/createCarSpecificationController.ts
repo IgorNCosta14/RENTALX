8a7a7a7ff4b9a45d0bb688c61d6fd8cbcee7a92a
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-04-04 19:53:21 -0300

    04.04

---
+++ src/modules/cars/useCases/createCarSpecification/createCarSpecificationController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+class CreateCarSpecificationController {
+  handle(request: Request, response: Response): Promise<Response> {}
+}
+
+export { CreateCarSpecificationController };
